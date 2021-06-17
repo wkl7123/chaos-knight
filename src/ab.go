2dc953231ff5fa9ecac203db069107f2d2ebf778
Iver <1932005951@qq.com>
2021-06-17 14:20:49 +0800

    add read conf func

--- src/ab.go
+++ src/ab.go
@@ -1,5 +1,11 @@
 package src
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
 type Conf map[string]Scene
 
 type Scene struct {
@@ -7,34 +13,29 @@ type Scene struct {
 }
 
 type FullLayers struct {
-	Recall  RecallLayer  `json:"recall"`
-	Feature FeatureLayer `json:"feature"`
-	Filter  FilterLayer  `json:"filter"`
-	Model   ModelLayer   `json:"model"`
-	Rerank  RerankLayer  `json:"rerank"`
+	Recall  []LayerOption `json:"recall"`
+	Feature []LayerOption `json:"feature"`
+	Filter  []LayerOption `json:"filter"`
+	Model   []LayerOption `json:"model"`
+	Rerank  []LayerOption `json:"rerank"`
 }
 
-type RecallLayer struct {
-	Options []Option `json:"options"`
-}
-
-type FeatureLayer struct {
-	Options []Option `json:"options"`
-}
-
-type FilterLayer struct {
-	Options []Option `json:"options"`
-}
-
-type ModelLayer struct {
-	Options []Option `json:"options"`
-}
-
-type RerankLayer struct {
-	Options []Option `json:"options"`
-}
-
-type Option struct {
+type LayerOption struct {
 	Prob   float32  `json:"prob"`
 	Choose []string `json:"choose"`
+}
+
+func GetConfFromFile(confPath string) (conf *Conf, err error) {
+	conf = &Conf{}
+	confData, err := ioutil.ReadFile("conf.json")
+	if err != nil {
+		err = fmt.Errorf("config is not valid json: err: %v", err)
+		return
+	}
+	err = json.Unmarshal(confData, conf)
+	if err != nil {
+		err = fmt.Errorf("conf not valid: err: %v", err)
+		return
+	}
+	return
 }
